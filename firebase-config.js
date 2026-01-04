577bd75006caf764ebad7d82b21a1a102222e6d2
Sayaz06 <adha86614@gmail.com>
2026-01-04 20:04:30 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,60 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithPopup,
+  signOut
+} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+import {
+  getFirestore,
+  collection,
+  doc,
+  addDoc,
+  setDoc,
+  getDoc,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  deleteDoc,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAODqvhbdpg7XYAjff_MapQ0xJYeanU4Cw",
+  authDomain: "stress-discussion.firebaseapp.com",
+  projectId: "stress-discussion",
+  storageBucket: "stress-discussion.firebasestorage.app",
+  messagingSenderId: "1028889010378",
+  appId: "1:1028889010378:web:10ad46920cc6039a195433",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
+
+// Export supaya boleh guna di file lain
+export {
+  app,
+  auth,
+  provider,
+  db,
+  onAuthStateChanged,
+  signInWithPopup,
+  signOut,
+  collection,
+  doc,
+  addDoc,
+  setDoc,
+  getDoc,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  deleteDoc,
+  serverTimestamp
+};
